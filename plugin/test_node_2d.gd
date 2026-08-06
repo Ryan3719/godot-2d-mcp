8b25ad229f5d64687dc206be21d383b3c8636e99
zhinian <1774813307@qq.com>
2026-08-06 20:27:51 +0800

    Support typed container properties

--- plugin/test_node_2d.gd
+++ plugin/test_node_2d.gd
@@ -1,3 +1,8 @@
 extends Node2D
 
 @export var agent_label := "scripted"
+@export var accent_colors: Array[Color] = []
+@export var color_labels: Dictionary[String, Color] = {}
+@export var color_lookup: Dictionary[Color, String] = {}
+@export var icon_textures: Array[Texture2D] = []
+@export var node_references: Array[Node] = []
